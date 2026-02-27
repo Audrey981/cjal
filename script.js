860f8bb7adce01b246ad3d89ad115f84f96fe493
Audrey981 <blondela1215@gmail.com>
2026-02-27 15:24:20 +0100

    taille blocs

--- script.js
+++ script.js
@@ -14,7 +14,24 @@ function goTo(i){
 document.querySelector('.slider-prev').addEventListener('click', () => goTo(index - 1));
 document.querySelector('.slider-next').addEventListener('click', () => goTo(index + 1));
 
+/* Ajout : synchroniser index quand on swipe/scroll à la main */
+function updateIndexFromScroll(){
+  const center = container.scrollLeft + container.clientWidth / 2;
+  let closest = 0;
+  let minDist = Infinity;
 
+  slides.forEach((slide, i) => {
+    const slideCenter = slide.offsetLeft + slide.clientWidth / 2;
+    const dist = Math.abs(center - slideCenter);
+    if (dist < minDist) { minDist = dist; closest = i; }
+  });
+
+  index = closest;
+}
+
+container.addEventListener('scroll', () => {
+  window.requestAnimationFrame(updateIndexFromScroll);
+});
 
 document.querySelectorAll('.faq-question').forEach(q => {
   q.addEventListener('click', () => {
